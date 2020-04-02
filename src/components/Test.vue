9c9660ce077768dca5464a120550ee8721c9d25a
陈建希 <1049602251@qq.com>
2020-04-02 08:52:25 +0800

    测试babel

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            name  :"cjx"
+        }
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: "home",
+        component: () => import("../components/HelloWorld")
+    }, {
+        path: '/name',
+        name: "name",
+        component: () => import('../components/Test')
+    }
+]
+const router = new VueRouter({
+    mode: "hash",
+    base: process.env.BASE_URL,
+    routes
+})
+export default router;
